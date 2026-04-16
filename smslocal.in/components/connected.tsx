c5ac40a99c922cd4044bf0215726ab0e91e43974
nishiex <nishitpatel.expertwise@gmail.com>
2026-04-17 00:24:38 +0530

    17-04-26 commit

---
+++ smslocal.in/components/connected.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function connected() {
+  return (
+    <div className="relative h-[500px] flex flex-col items-center justify-center bg-[url('https://ik.imagekit.io/nishit560/Desktop%20-%201.png')] bg-cover bg-center">
+
+<div class="absolute inset-0 bg-gradient-to-b from-white to-[#f5f5f5] opacity-70"></div>
+<h1 className='font-PlusJakarta z-1 text-[48px] md:text-[55px] H-[500px] text-[#034737] font-bold leading-tight '>
+    Connected with Direct Operator
+</h1>
+
+ <div className='flex justify-center w-[1240px] items-center  z-1 '>
+<div >
+<img src="https://www.smslocal.in/wp-content/uploads/2024/07/1-1.png" alt="" srcset="" />
+
+</div>
+<div>
+
+<img src="https://www.smslocal.in/wp-content/uploads/2024/07/1.png" alt="" srcset="" />
+</div>
+<div>
+
+  <img src="https://www.smslocal.in/wp-content/uploads/2024/07/2-1.png" alt="" srcset="" />
+</div>
+<div>
+
+ <img src="https://www.smslocal.in/wp-content/uploads/2024/07/5.png" alt="" srcset="" />
+</div>
+ <div>
+
+ <img src="https://www.smslocal.in/wp-content/uploads/2024/07/3.png" alt="" srcset="" />
+ </div>
+ </div>
+
+    </div>
+  )
+}
+
+export default connected
--- smslocal.in/components/home.tsx
+++ smslocal.in/components/home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Hero from "./hero";
+import Connected from "./connected";
 const home = () => {
   return (
   <>
@@ -80,55 +81,10 @@ const home = () => {
   <button className="text-[12px] text-white bg-[#1C3F39] pr-[10px] pl-[10px] pt-2 pb-2 rounded-[2px]">₹60 Free Credit</button>
 </div>
   </div>
-
   {/* Hero Section */}
-  <div className="bg-[linear-gradient(180deg,#FFFFFF_50%,#FAFAFA_50%)] md:bg-[linear-gradient(90deg,#FFFFFF_50%,#FAFAFA_50%)] flex flex-col md:flex-row items-center px-4 md:px-6 py-10 md:py-[70px] gap-8">
+   <Hero />
+   <Connected />
 
-    {/* Left Content */}
-    <div className="w-full md:w-1/2">
-      <h1 className="font-PlusJakarta text-[28px] md:text-[55px] text-black font-bold leading-tight">
-        Signup & Try Bulk SMS Service For <span>Free</span>
-      </h1>
-
-      <p className="text-[14px] md:text-[16px] text-black mt-3 md:w-[80%]">
-        Send Promotional, Transactional & OTP SMS — Fully DLT compliant with API support
-      </p>
-
-      <div className="mt-4 border-l-[3px] border-[#ff0000] pl-3">
-        <p className="text-[13px] text-black">Test Our Free Bulk Sms Service</p>
-        <span className="font-bold text-[#ff0000] text-[14px]">
-          Get ₹60 Free Credit On Signup
-        </span>
-      </div>
-
-      <button className="mt-5 flex items-center gap-2 px-5 py-3 bg-[#013831] rounded-md text-[13px] md:text-[14px] text-white font-bold">
-        Get ₹60 Free Credits Now!
-        <svg width="12" fill="#ffffff" viewBox="0 0 448 512">
-          <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
-        </svg>
-      </button>
-
-      <button className="mt-5 flex items-center gap-2 px-5 py-3 bg-[#ffffff] text-black rounded-md text-[13px] broder-2 broder-[#013830] md:text-[14px] text-white font-bold">
-        pricing
-        <svg width="12" fill="#ffffff" viewBox="0 0 448 512">
-          <path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path>
-        </svg>
-      </button>
-    </div>
-
-    {/* Right Image */}
-    <div className="w-full md:w-1/2">
-      <img
-        src="https://www.smslocal.in/wp-content/uploads/2025/08/dashboard-1111-3-2048x1314.webp"
-        className="w-full h-auto rounded-md "
-        alt="dashboard"
-      />
-    </div>
-  </div>
-
-<div>
-  
-</div>
   
 </>
   
